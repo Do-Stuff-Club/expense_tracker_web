93151e04edbbf872c104b2b047f0aaf780358af1
Kenneth Fang <fangkw42@gmail.com>
2021-02-20 14:50:27 -0500

    Forgot to add dashboard and api TT

---
+++ api/tag/types.ts
@@ -0,0 +1,52 @@
+import { Category, Tag } from "../../redux/tags/types";
+import { AuthHeaders } from "../../redux/user/types";
+
+export interface GetTagParams {
+    user_id: number;
+    headers: AuthHeaders;
+}
+
+export interface CreateCategoryParams {
+    name: string;
+    required: boolean;
+    tags: ReadonlyArray<string>;
+    headers: AuthHeaders;
+}
+
+export interface DeleteCategoryParams {
+    id: number;
+    headers: AuthHeaders;
+}
+
+export interface UpdateCategoryParams {
+    category: Category;
+    headers: AuthHeaders;
+}
+
+export interface CreateTagParams {
+    name: string;
+    category: Category;
+    headers: AuthHeaders;
+}
+
+export interface DeleteTagParams {
+    id: number;
+    category: Category;
+    headers: AuthHeaders;
+}
+
+export interface UpdateTagParams {
+    tag: Tag;
+    category: Category;
+    headers: AuthHeaders;
+}
+
+export interface AllCategoriesData {
+    authHeaders: AuthHeaders;
+    categories: ReadonlyArray<Category>
+}
+
+export interface OneCategoryData {
+    authHeaders: AuthHeaders;
+    category: Category;
+}
