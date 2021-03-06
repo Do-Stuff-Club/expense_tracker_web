65413fc01335a5865fb5eb78f95f182e8ecd5fd2
Mena Wang <wang.mena@gmail.com>
2021-03-06 14:15:03 -0800

    formik

--- pages/sign_up.tsx
+++ pages/sign_up.tsx
@@ -11,6 +11,8 @@ import { newUserCall } from '../api/user/call';
 import { NewUserParams } from '../api/user/types';
 import { loginAction } from '../redux/user/action';
 
+import { useFormik } from 'formik';
+
 const connector = connect(null, {
     loginAction,
 });
@@ -18,56 +20,80 @@ type ReduxProps = ConnectedProps<typeof connector>;
 type SignUpProps = ReduxProps;
 
 export function SignUp(props: SignUpProps) {
-    const [state, setState] = useState<NewUserParams>({
-        email: '',
-        password: '',
-        password_confirmation: '',
+    
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            password_confirmation: '',
+        },
+        onSubmit: (values) => {
+            newUserCall(values).then(
+                (data) => {
+                    props.loginAction(data);
+                },
+                (error) => {
+                    console.log(error);
+                },
+            );
+
+            //event.preventDefault();
+        },
     });
+    
+    // const [state, setState] = useState<NewUserParams>({
+    //     email: '',
+    //     password: '',
+    //     password_confirmation: '',
+    // });
     const router = useRouter();
 
-    const handleChange = (
-        name: string,
-        event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-    ) => {
-        const target = event.target;
-        setState({
-            ...state,
-            [name]: target.value,
-        });
-    };
+    // const handleChange = (
+    //     name: string,
+    //     event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    // ) => {
+    //     const target = event.target;
+    //     setState({
+    //         ...state,
+    //         [name]: target.value,
+    //     });
+    // };
 
-    const handleSubmit = (event: SyntheticEvent) => {
-        newUserCall(state).then(
-            (data) => {
-                props.loginAction(data);
-            },
-            (error) => {
-                console.log(error);
-            },
-        );
-        event.preventDefault();
-    };
+    // const handleSubmit = (event: SyntheticEvent) => {
+    //     newUserCall(state).then(
+    //         (data) => {
+    //             props.loginAction(data);
+    //         },
+    //         (error) => {
+    //             console.log(error);
+    //         },
+    //     );
+    //     event.preventDefault();
+    // };
 
     return (
         <PageLayout pageName='Sign Up'>
             <h1>Sign Up</h1>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={formik.handleSubmit}>
                 <TextField
                     label='Email'
                     variant='filled'
-                    onChange={(e) => handleChange('email', e)}
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
                 />
                 <br />
                 <TextField
                     label='Password'
                     variant='filled'
-                    onChange={(e) => handleChange('password', e)}
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
                 />
                 <br />
                 <TextField
                     label='Password Confirmation'
                     variant='filled'
-                    onChange={(e) => handleChange('password_confirmation', e)}
+                    onChange={formik.handleChange}
+                    value={formik.values.password_confirmation}
                 />
                 <br />
                 <Button variant='contained' type='submit'>
