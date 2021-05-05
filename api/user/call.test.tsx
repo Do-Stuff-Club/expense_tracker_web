8e87dc0e36387709a8eeb59e9f834ac102806d7c
Kenneth Fang <fangkw42@gmail.com>
2021-05-04 21:53:20 -0400

    Adding user API tests (basic)

--- api/user/call.test.tsx
+++ api/user/call.test.tsx
@@ -1,9 +1,9 @@
 // ===================================================================
 //                             Imports
 // ===================================================================
-import { NewUserParams, UserData } from './types';
+import { LoginParams, NewUserParams, UserData } from './types';
 import axios from 'axios';
-import { newUserCall } from './call';
+import { loginCall, newUserCall } from './call';
 
 // ===================================================================
 //                              Tests
@@ -24,12 +24,12 @@ test('newUserCall() successfully creates a user', async () => {
         statusText: 'OK',
         config: {},
         headers: {
+            'Content-Type': 'application/json; charset=utf-8',
             'access-token': '-MW9nVhXviMt83nlYQU9yw',
             'token-type': 'Bearer',
             client: 'VZ6QbHPUroBvLnVcKQGYkw',
             expiry: '1618718924',
             uid: 'test@test.org',
-            'Content-Type': 'application/json; charset=utf-8',
         },
         data: {
             status: 'success',
@@ -59,4 +59,102 @@ test('newUserCall() successfully creates a user', async () => {
     mockedAxios.mockResolvedValueOnce(mockApiResponse);
 
     await expect(newUserCall(testParams)).resolves.toEqual(expectedData);
+});
+
+test('newUserCall() returns an error on non-200 messages', async () => {
+    const testParams: NewUserParams = {
+        email: 'test@domain.com',
+        password: 'super_secret',
+        password_confirmation: 'super_secret',
+    };
+
+    // See documentation generated here: https://github.com/kwf37/expense_tracker_api
+    const mockApiResponse = {
+        status: 401,
+        statusText: 'Bad Request',
+        config: {},
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+        },
+        data: {
+            response: 'error',
+        },
+    };
+
+    mockedAxios.mockRejectedValueOnce(mockApiResponse);
+
+    await expect(newUserCall(testParams)).rejects.toEqual(mockApiResponse);
+});
+
+test('loginCall() successfully returns user data', async () => {
+    const testParams: LoginParams = {
+        email: 'test@domain.com',
+        password: 'super_secret',
+    };
+
+    // See documentation generated here: https://github.com/kwf37/expense_tracker_api
+    const mockApiResponse = {
+        status: 200,
+        statusText: 'OK',
+        config: {},
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'access-token': '-MW9nVhXviMt83nlYQU9yw',
+            'token-type': 'Bearer',
+            client: 'VZ6QbHPUroBvLnVcKQGYkw',
+            expiry: '1618718924',
+            uid: 'test@test.org',
+        },
+        data: {
+            status: 'success',
+            data: {
+                uid: 'test@domain.com',
+                id: 1,
+                email: 'test@domain.com',
+                provider: 'email',
+                allow_password_change: false,
+                created_at: '2021-04-04T04:08:44.671Z',
+                updated_at: '2021-04-04T04:08:44.680Z',
+            },
+        },
+    };
+
+    const expectedData: UserData = {
+        id: 1,
+        authHeaders: {
+            'access-token': '-MW9nVhXviMt83nlYQU9yw',
+            'token-type': 'Bearer',
+            client: 'VZ6QbHPUroBvLnVcKQGYkw',
+            expiry: '1618718924',
+            uid: 'test@test.org',
+        },
+    };
+
+    mockedAxios.mockResolvedValueOnce(mockApiResponse);
+
+    await expect(loginCall(testParams)).resolves.toEqual(expectedData);
+});
+
+test('loginCall() returns an error on non-200 messages', async () => {
+    const testParams: LoginParams = {
+        email: 'test@domain.com',
+        password: 'super_secret',
+    };
+
+    // See documentation generated here: https://github.com/kwf37/expense_tracker_api
+    const mockApiResponse = {
+        status: 401,
+        statusText: 'Bad Request',
+        config: {},
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+        },
+        data: {
+            response: 'error',
+        },
+    };
+
+    mockedAxios.mockRejectedValueOnce(mockApiResponse);
+
+    await expect(loginCall(testParams)).rejects.toEqual(mockApiResponse);
 });
