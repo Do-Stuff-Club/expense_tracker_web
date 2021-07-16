589089d1df21b85e4d8120ac5285d10e35004cf5
Kenneth Fang <kwf37@cornell.edu>
2021-07-15 21:12:13 -0400

    Adding new tree structure

--- temp_data/tree.ts
+++ temp_data/tree.ts
@@ -7,7 +7,7 @@ export interface TagTree {
 
 export interface TagState {
     map: Map<number, TagTree>;
-    rootIds: ReadonlyArray<number>
+    rootIds: ReadonlyArray<number>;
 }
 
 // === Crafting tree ===
@@ -16,51 +16,51 @@ export const crafts: TagTree = {
     name: 'Crafts',
     id: 0,
     childrenIds: [1, 2],
-}
+};
 
 export const sewing: TagTree = {
     name: 'Sewing',
     id: 1,
     parentId: 0,
-}
+};
 
 export const crochet: TagTree = {
     name: 'Crocheting',
     id: 2,
     parentId: 0,
-}
+};
 
 // === Utilities tree ===
 export const utils: TagTree = {
     name: 'Utilities',
     id: 3,
-    childrenIds: [4, 5, 6]
-}
+    childrenIds: [4, 5, 6],
+};
 
 export const elec: TagTree = {
     name: 'electricity',
     id: 4,
     parentId: 3,
-    childrenIds: [7]
-}
+    childrenIds: [7],
+};
 
 export const gas: TagTree = {
     name: 'gas',
     id: 5,
     parentId: 3,
-}
+};
 
 export const internet: TagTree = {
     name: 'internet',
     id: 6,
     parentId: 3,
-}
+};
 
 export const solar: TagTree = {
     name: 'solar',
     parentId: 4,
     id: 7,
-}
+};
 
 // Exported tree
 export const dummyTree: TagState = {
@@ -74,5 +74,5 @@ export const dummyTree: TagState = {
         [internet.id, internet],
         [solar.id, solar],
     ]),
-    rootIds: [crafts.id, utils.id]
-}
+    rootIds: [crafts.id, utils.id],
+};
