b7155ede1b07f2c9563cf1f9b6b6e52352c67321
Mena Wang <wang.mena@gmail.com>
2021-07-20 18:53:43 -0700

    tags displaying

---
+++ components/tags.tsx
@@ -0,0 +1,45 @@
+// ===================================================================
+//                             Imports
+// ===================================================================
+import React, { useState } from 'react';
+import TreeItem from '@material-ui/lab/TreeItem';
+import TreeView from '@material-ui/lab/TreeView';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import * as TagData from '../temp_data/tree';
+import { TagState, TagTree } from '../temp_data/tree';
+
+/**
+ *
+ * @returns {Element} A Tree view of all tags
+ */
+export default function Tags(): JSX.Element {
+    const [tagState, setTagState] = useState<TagState>(TagData.dummyTree);
+
+    const renderTree = (nodes: TagTree) => (
+        <TreeItem
+            key={nodes.id}
+            nodeId={nodes.id.toString()}
+            label={nodes.name}
+        >
+            {console.log(nodes.name)}
+            {Array.isArray(nodes.childrenIds)
+                ? nodes.childrenIds.map((childrenId) =>
+                      renderTree(tagState.map.get(childrenId) as TagTree),
+                  )
+                : null}
+        </TreeItem>
+    );
+
+    return (
+        <TreeView
+            defaultCollapseIcon={<ExpandMoreIcon />}
+            defaultExpandIcon={<ChevronRightIcon />}
+        >
+            {tagState.rootIds.map((id) => {
+                console.log(tagState.map.get(id));
+                return renderTree(tagState.map.get(id) as TagTree);
+            })}
+        </TreeView>
+    );
+}
