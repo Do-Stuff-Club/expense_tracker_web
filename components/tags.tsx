91d6b3b3e0b64dc941bb5ebf9c2165b7542e0806
vi35 <vi35@cornell.edu>
2021-08-04 19:28:56 -0600

    create new tags as root nodes

--- components/tags.tsx
+++ components/tags.tsx
@@ -1,21 +1,25 @@
 // ===================================================================
 //                             Imports
 // ===================================================================
-import React from 'react'; //add useState back in
+import React, { useState } from 'react';
 import TreeItem from '@material-ui/lab/TreeItem';
 import TreeView from '@material-ui/lab/TreeView';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import * as TagData from '../temp_data/tree';
-import { TagTree } from '../temp_data/tree'; //add TagState back in
+import { TagTree, TagState } from '../temp_data/tree'; //add TagState back in
+import { TextField, Button } from '@material-ui/core';
+// import FormButton from './formButton';
 
 /**
  *
  * @returns {Element} A Tree view of all tags
  */
 export default function Tags(): JSX.Element {
-    //const [tagState, setTagState] = useState<TagState>(TagData.dummyTree);
-    const tagState = TagData.dummyTree;
+    const [tagState, setTagState] = useState<TagState>(TagData.dummyTree);
+    // const [currentSelection, setCurrentSelection] = useState<string>('');
+    const [newTagName, setNewTagName] = useState<string>('');
+    // const tagState = TagData.dummyTree;
 
     const renderTree = (nodes: TagTree) => (
         <TreeItem
@@ -32,15 +36,44 @@ export default function Tags(): JSX.Element {
         </TreeItem>
     );
 
+    const addNode = (nodeName: string) => {
+        const newId = tagState.map.size;
+        const newNode: TagTree = {
+            name: nodeName,
+            id: newId,
+        };
+
+        setTagState({
+            map: tagState.map.set(newId, newNode),
+            rootIds: [...tagState.rootIds, ...[newId]],
+        });
+
+        setNewTagName('');
+    };
+
     return (
-        <TreeView
-            defaultCollapseIcon={<ExpandMoreIcon />}
-            defaultExpandIcon={<ChevronRightIcon />}
-        >
-            {tagState.rootIds.map((id) => {
-                console.log(tagState.map.get(id));
-                return renderTree(tagState.map.get(id) as TagTree);
-            })}
-        </TreeView>
+        <div>
+            <div>
+                <span>Add a new tag: </span>
+                <TextField
+                    value={newTagName}
+                    onChange={(event) =>
+                        setNewTagName(
+                            event.target.value ? event.target.value : '',
+                        )
+                    }
+                ></TextField>
+                <Button onClick={() => addNode(newTagName)}>+</Button>
+            </div>
+            <TreeView
+                defaultCollapseIcon={<ExpandMoreIcon />}
+                defaultExpandIcon={<ChevronRightIcon />}
+            >
+                {tagState.rootIds.map((id) => {
+                    console.log(tagState.map.get(id));
+                    return renderTree(tagState.map.get(id) as TagTree);
+                })}
+            </TreeView>
+        </div>
     );
 }
