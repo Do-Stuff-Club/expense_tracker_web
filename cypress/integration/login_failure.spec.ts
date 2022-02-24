cdfa5ffd1bc2fd10c83bcfe5a157a795be6b8194
Varun <vi35@cornell.edu>
2022-02-24 00:52:14 -0700

    add login integration tests

---
+++ cypress/integration/login_failure.spec.ts
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+context('Network Requests', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/login');
+    })
+
+    it('fails to log in given incorrect login info', () => {
+        // https://on.cypress.io/intercept
+    
+        // Intercept - can be used for future spying
+        cy.intercept('POST', '**/auth/sign_in*').as('postLoginResponse')
+    
+        // we have code that enters login info
+        cy.findByLabelText('Email', { timeout: 7500 })
+        .type('tomato@tomato.com').should('have.value', 'tomato@tomato.com');
+
+        cy.findByLabelText('Password', { timeout: 7500 })
+        .type('asdfasdf').should('have.value', 'asdfasdf');
+    
+        // we have code that clicks the login button
+        cy.get('button[type=submit]').findByText('Log In', { timeout: 7500 }).click();
+
+        // https://on.cypress.io/wait
+        cy.wait('@postLoginResponse').its('response.statusCode').should('be.oneOf', [401])
+
+        // we don't navigate anywhere given incorrect login info
+        cy.location('pathname').should('include', '/login')
+      })
+
+})
---
+++ cypress/integration/signup_nav.spec.ts
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+context('Navigation', () => {
+    beforeEach(() => {
+        // Cypress starts out with a blank slate for each test
+        // so we must tell it to visit our website with the `cy.visit()` command.
+        // Since we want to visit the same URL at the start of all our tests,
+        // we include it in our beforeEach function so that it runs before each test
+        cy.visit('http://localhost:3000');
+    });
+
+    it('Navigates to the sign up page when sign up is pressed', () => {
+        
+        // we click login
+        cy.findByText('Sign Up', { timeout: 7500 }).click();
+
+        // we should be at /login
+        cy.location('pathname').should('include', '/signup')
+    });
+});
